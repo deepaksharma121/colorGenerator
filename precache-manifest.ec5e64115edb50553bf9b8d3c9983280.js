17d6f6411b31fb7cac193852b11b2ee3d2a6f842
Vicky <vicky@Deepaks-MacBook-Air.local>
2024-11-10 23:26:08 -0600

    Updates

---
+++ precache-manifest.ec5e64115edb50553bf9b8d3c9983280.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0152eac586edcc3b06cdb39fd1199d4",
+    "url": "/colorGenerator/index.html"
+  },
+  {
+    "revision": "962e71f0f47c777fda5c",
+    "url": "/colorGenerator/static/css/main.0e47d3d7.chunk.css"
+  },
+  {
+    "revision": "fa270ff5dc62afe8ec50",
+    "url": "/colorGenerator/static/js/2.4838dfb8.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/colorGenerator/static/js/2.4838dfb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf7efe6a2eab99d5c1d9",
+    "url": "/colorGenerator/static/js/3.7de085b8.chunk.js"
+  },
+  {
+    "revision": "962e71f0f47c777fda5c",
+    "url": "/colorGenerator/static/js/main.481cfaa1.chunk.js"
+  },
+  {
+    "revision": "1a3e2c0b204011c753fe",
+    "url": "/colorGenerator/static/js/runtime-main.ff056b3d.js"
+  }
+]);
